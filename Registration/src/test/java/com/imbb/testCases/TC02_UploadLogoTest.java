5a2877cf3ce4604a00cdd2c42caf2c459c283ba8
Vikram@Vikram-PC <kv.vikram15@gmail.com>
2019-03-21 00:55:06 +0100

    latest update

--- Registration/src/test/java/com/imbb/testCases/TC02_UploadLogoTest.java
+++ Registration/src/test/java/com/imbb/testCases/TC02_UploadLogoTest.java
@@ -10,7 +10,7 @@ import junit.framework.Assert;
 
 public class TC02_UploadLogoTest extends BaseClass {
 
-	@Test(groups = {"B"}, dependsOnGroups = "A")
+	@Test(groups = {"B"})
 	public void uploadPicture() throws Exception {
 		
 		//Login url is called for the logging to application after registration
